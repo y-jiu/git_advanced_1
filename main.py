ff81638c95b43d06fc2253130ccf9f395be3d9ad
y-jiu <chldudwn99@gmail.com>
2023-10-18 18:17:36 +0900

    Implement even_list

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def even_list(int_list: List[int]) -> List[int]:
     Returns:
         A list of even integers.
      """
-    # TODO: Implement even_list
-    pass
+    even_int_list = [x for x in int_list if x % 2 == 0]
+    return even_int_list
 
 
 # Skeleton code for sum_of_squares_of_even
